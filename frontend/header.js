31b6493f88014defac6aef207b77866de48916f1
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2024-03-30 01:19:17 +0530

    color scheme updated for landing, about us and contact page

---
+++ frontend/header.js
@@ -0,0 +1,66 @@
+document.write(`
+<header class="tp-header-height">
+<div id="header-sticky" class="header-bottom__area header-bottom__plr-5 header-bottom__transparent z-index-3 white-bg">
+   <div class="container-fluid p-0">
+      <div class="row g-0 align-items-center">
+         <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-6">
+            <div class="header-bottom__logo">
+               <a href="index.html"><img src="assets/logo_with_full_name.svg" alt=""></a>
+            </div>
+         </div>
+         <div class="col-xxl-8 col-xl-7 col-lg-8 d-none d-lg-block">
+            <div class="header-bottom__main-menu header-bottom__main-menu-5 text-center">
+               <nav id="mobile-menu">
+                  <ul>
+                     <li>
+                        <a href="index.html">Home</a>
+                     </li>
+                     <li><a href="about.html">About Us</a></li>
+                     <li><a href="contact.html">Contact Us</a></li>
+                  </ul>
+               </nav>
+            </div>
+         </div>
+         <div class="col-xxl-2 col-xl-3 col-lg-2 col-md-8 col-6">
+            <div class="header-bottom__right header-five__btn d-flex align-items-center justify-content-end">
+               <div class="header-bottom__btn d-flex align-items-center">
+                  <a class="header-bottom__bar tp-menu-bar d-lg-none" href="javascript:void(0)"><i class="fal fa-bars"></i></a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<div class="tpoffcanvas-area">
+   <div class="tpoffcanvas">
+      <div class="tpoffcanvas__close-btn">
+         <button class="close-btn"><i class="fal fa-times"></i></button>
+      </div>
+      <div class="tpoffcanvas__logo text-center">
+         <a href="index.html">
+            <img src="assets/logo_with_full_name.svg" alt="">
+         </a>
+      </div>
+      <div class="mobile-menu"></div>
+      <div class="tpoffcanvas__info text-center">
+         <h4 class="offcanva-title">we are here</h4>
+         <a href="https://maps.app.goo.gl/fhizeEyeMJHnWQq29" target="_blank">
+            Seawoods, Navi Mumbai, Maharastra, India
+         </a>
+      </div>
+      <div class="tpoffcanvas__social">
+         <div class="social-icon text-center">
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-facebook-square"></i></a>
+            <a href="#"><i class="fab fa-dribbble"></i></a>
+         </div>
+      </div>
+   </div>
+</div>
+<div class="body-overlay"></div>
+
+
+</header>
+`)
